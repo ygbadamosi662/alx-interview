b2d35f243d0d378ee5e084e7befff88aff7256b8
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-08-17 14:37:12 +0100

    okay

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,64 +19,50 @@ status codes should be printed in ascending order
 line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
 <status code>, <file size>]
 """
-import re
+
+
 import sys
 
+# store the count of all status codes in a dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
-def increment(on_the_plus_side, x):
-    """
-    Defines function increment, increments on_the_plus_side by
-    x.
-    Args:
-    on_the_plus_side: int
-    x: int or str
-    Return: int, sum of x and on_the_plus_side
-    """
-    return on_the_plus_side + int(x)
-
+total_size = 0
+count = 0  # keep count of the number lines counted
 
 try:
-    p = r'^[\d.]+ - \[[^\]]+\] "GET \/projects\/\d+ HTTP\/1\.1" \d{3} \d+$'
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                    '403': 0, '404': 0, '405': 0, '500': 0}
-    print_out = {'file_size': 0, 'status_codes': status_codes}
-    i = 0
-    j = 0
-
-    def print_shit():
-        """
-        Defines function print_shit, prints the dict print_out
-        in the below format:
-        File Size: <total file_size>
-        status code: code
-        ...
-
-        Return: does not return anything
-        """
-        print('File Size: {}'.format(print_out.get('file_size')))
-        sorted_keys = sorted(print_out['status_codes'].keys())
-
-        for key in sorted_keys:
-            val = print_out['status_codes'].get(key)
-            if val != 0:
-                print('{}: {}'.format(key, val))
-
     for line in sys.stdin:
-        j += 1
-        if re.match(p, line):
-            arr = line.split(' ')
-            olodos = arr[-2:]
-            status = olodos[0]
-            file_size = olodos[1]
-            print_out['file_size'] = increment(print_out.get('file_size'),
-                                               file_size)
+        line_list = line.split(" ")
 
-            if status.isdigit():
-                pstatus = print_out['status_codes']
-                pstatus[status] = increment(pstatus.get(status), 1)
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-        if (j - i) == 10:
-            i = j
-            print_shit()
-except KeyboardInterrupt:
-    print_shit()
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
+
+            # update total size
+            total_size += file_size
+
+            # update count of lines
+            count += 1
+
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
+
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
